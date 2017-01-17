1f3d1cdd4a629e3cb42e62687011c8ba594edcf8
AnneliektH <anneliekterhorst@gmail.com>
2017-01-17 12:59:22 +0100

    fixed self avoiding walks

---
+++ algorithms.py
@@ -0,0 +1,40 @@
+import random
+from Protein import *
+
+def initialize():
+	"""
+	Generates a pseudorandom self avoiding walk of the input sequence
+	"""
+
+	sequence = ["H", "P", "H", "H", "P","H", "P", "H", "H", "P", "H", ]
+	locations = [(5,5,5)]
+
+	for i in range(len(sequence)):
+		while True:
+			n = random.randint(0, 6)
+			if n == 1:
+				next = (locations[i][0],locations[i][1] - 1,locations[i][2])
+			elif n == 2:
+				next = (locations[i][0],locations[i][1] + 1,locations[i][2])
+			elif n == 3:
+				next = (locations[i][0],locations[i][1],locations[i][2] - 1)
+			elif n == 4:
+				next = (locations[i][0],locations[i][1],locations[i][2] + 1)
+			elif n == 5:
+				next = (locations[i][0] - 1,locations[i][1],locations[i][2])
+			else:
+				next = (locations[i][0] + 1,locations[i][1],locations[i][2])
+			
+			# avoids self intersections
+			if next not in locations:
+				break
+
+		locations.append(next)
+
+	# generate and visualize	
+	protein = Protein(sequence,locations)
+	protein.find_neighbours()
+	protein.calculate_stability()
+
+	protein.visualize()
+
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 # imports
 from Protein import *
 from Amino_acid import *
-	
+from algorithms import *
 
-sequence = ["H", "P", "H", "H", "P","H", "P", "H", "H", "P", "H"]
-locations = [(1,1,0),(0,1,0),(0,1,1),(1,1,1),(2,1,1),(2,1,2),(2,1,3),(2,1,4),(2,2,4),(2,2,3),(2,2,2)]
+# sequence = ["H", "P", "H", "H", "P","H", "P", "H", "H", "P", "H"]
+# locations = [(1,1,0),(0,1,0),(0,1,1),(1,1,1),(2,1,1),(2,1,2),(2,1,3),(2,1,4),(2,2,4),(2,2,3),(2,2,2)]
 
-test = Protein(sequence, locations)
+initialize()
 
-test.find_neighbours()
-test.calculate_stability()
-
-test.visualize()
 
